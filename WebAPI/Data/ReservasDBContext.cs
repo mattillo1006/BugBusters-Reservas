1f913ea7561c679e4230f4dbe1b690463336763e
Matthew rivera <mariveraca@est.utn.ac.cr>
2026-09-18 17:00:23 -0600

    AB#1 clase de "contexto" para trabajar el mapeo de la DB y su posterior conexion

---
+++ WebAPI/Data/ReservasDBContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebAPI.Models;
+
+namespace WebAPI.Data
+{
+    public class ReservasDBContext : DbContext
+    {
+        public ReservasDBContext(DbContextOptions<ReservasDBContext> options) : base(options){ }
+        //DbSet es una "Representacion" de las tablas de BD indicandoles el tipo que deberian ser segun el codigo
+        public DbSet<Usuario> Usuarios{ get; set; }
+        public DbSet<Rol> Roles { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //mapear las tablas de la BD para asegurarnos que sean iguales segun la entidad
+            modelBuilder.Entity<Usuario>().ToTable("Usuarios");
+            modelBuilder.Entity<Rol>().ToTable("Roles");
+
+            modelBuilder.Entity<Usuario>().HasOne(u => u.Rol).WithMany().HasForeignKey(u => u.RolId);
+
+        }
+    }
+}
