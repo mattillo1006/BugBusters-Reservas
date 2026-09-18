aa3a6d115e96e9eda44fcd141bd489dbf37254ec
Matthew rivera <mariveraca@est.utn.ac.cr>
2026-09-18 16:24:06 -0600

    AB#1 Creacion de las clases basadas en las tablas de la BD que se usaran en el inicio de sesion (fue rapido porque solo fue copiar y pegar de lo que ya hicimos en la BD)

---
+++ WebAPI/Models/Rol.cs
@@ -0,0 +1,9 @@
+﻿namespace WebAPI.Models
+{
+    public class Rol
+    {
+        public int RolId { get; set; }
+        public string NombreRol { get; set; } = string.Empty;
+        public string? Descripcion { get; set; }
+    }
+}
---
+++ WebAPI/Models/Usuario.cs
@@ -0,0 +1,17 @@
+﻿namespace WebAPI.Models
+{
+    public class Usuario
+    {
+        public int UsuarioId { get; set; }
+        public string NombreUsuario { get; set; } = string.Empty;
+        public string NombreCompleto { get; set; } = string.Empty;
+        public string Correo { get; set; } = string.Empty;
+        public string PasswordHash { get; set; } = string.Empty;
+        public int RolId { get; set; }
+        public bool Activo { get; set; }
+        public DateTime FechaRegistro { get; set; }
+
+        public Rol? Rol { get; set; }
+
+    }
+}
