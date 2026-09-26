4be79acb8ca8510f9420137c731159f5d87e201c
Matthew rivera <mariveraca@est.utn.ac.cr>
2026-09-26 12:15:21 -0600

    AB#2 creacion de la entidad laboratorio basada en la bd y un respectivo DTO para la posterior visualizacion de los laboratorios

---
+++ WebAPI/DTOs/LaboratorioResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace WebAPI.DTOs;
+public class LaboratorioResponse
+{
+    public int LaboratorioId { get; set; }
+    public string Nombre { get; set; } = string.Empty;
+    public string Ubicacion { get; set; } = string.Empty;
+    public int Capacidad { get; set; }
+    public string Estado { get; set; } = string.Empty;
+}
---
+++ WebAPI/Models/Laboratorio.cs
@@ -0,0 +1,13 @@
+﻿namespace ReservasLaboratorios.Api.Models;
+
+public class Laboratorio
+{
+    public int LaboratorioId { get; set; }
+    public string Nombre { get; set; } = string.Empty;
+    public string Ubicacion { get; set; } = string.Empty;
+    public int Capacidad { get; set; }
+    public string Estado { get; set; } = string.Empty; 
+    public TimeSpan HoraApertura { get; set; }
+    public TimeSpan HoraCierre { get; set; }
+    public bool Active { get; set; }
+}
