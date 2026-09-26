fcc83fd338e28aaf500df08b4bb4f5bdb17d97a4
Matthew rivera <mariveraca@est.utn.ac.cr>
2026-09-26 12:51:57 -0600

    AB#2 creacion de los endpoints de la api funcionales y enlazados con la info de la BD

---
+++ WebAPI/Controllers/LaboratoriosController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebAPI.Data;
+using WebAPI.DTOs;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LaboratoriosController : ControllerBase
+    {
+        private readonly ReservasDBContext _context;
+
+        public LaboratoriosController(ReservasDBContext context)
+        {
+            _context = context;
+        }
+        // Get: api/Laboratorios
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<LaboratorioResponse>>> GetAll()
+        {
+            var laboratorios = await _context.Laboratorios
+                .Where(l => l.Active)
+                .Select(l=> new LaboratorioResponse 
+            {
+                LaboratorioId = l.LaboratorioId,
+                Nombre = l.Nombre,
+                Ubicacion = l.Ubicacion,
+                Capacidad = l.Capacidad,
+                Estado = l.Estado
+            }).ToListAsync();
+            return Ok(laboratorios);
+        }
+
+        //Get: api/Laboratorios/id
+        [HttpGet("{id}")]
+        public async Task<ActionResult<LaboratorioResponse>> GetById(int id)
+        {
+            var laboratorio = await _context.Laboratorios
+                              .Where(l => l.LaboratorioId == id && l.Active)
+                              .Select(l => new LaboratorioResponse
+                              { 
+                              LaboratorioId= l.LaboratorioId,
+                              Nombre= l.Nombre,
+                              Ubicacion= l.Ubicacion,
+                              Capacidad= l.Capacidad,
+                              Estado= l.Estado
+                              }).FirstOrDefaultAsync();
+
+            if (laboratorio is null)
+            {
+                return NotFound(new { mensaje = "Laboratorio no encontrado" });
+            }
+            return Ok(laboratorio);
+        }
+    }
+}
